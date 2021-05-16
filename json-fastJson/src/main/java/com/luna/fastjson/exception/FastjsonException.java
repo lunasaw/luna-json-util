3ba10f74abc2625dc32b05671b6655acf1645c01
luna <iszychen@gmail.com>
2021-05-16 16:24:50 +0800

    init code

---
+++ json-fastJson/src/main/java/com/luna/fastjson/exception/FastjsonException.java
@@ -0,0 +1,25 @@
+package com.luna.fastjson.exception;
+
+import exception.FormativeException;
+
+/**
+ * @author duanxinyuan
+ * 2019/4/10 22:35
+ */
+public class FastjsonException extends FormativeException {
+    public FastjsonException() {
+        super();
+    }
+
+    public FastjsonException(String message) {
+        super(message);
+    }
+
+    public FastjsonException(Throwable cause) {
+        super(cause);
+    }
+
+    public FastjsonException(String format, Object... arguments) {
+        super(format, arguments);
+    }
+}
